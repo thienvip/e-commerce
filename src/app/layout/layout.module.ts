411962ccfeba59718e433246b0f16f773c3b9546
Your Name <qvthien6@gmail.com>
2023-06-09 17:09:43 +0700

    updated

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HeaderComponent } from '../components/partials/header/header.component';
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LayoutModule { }
