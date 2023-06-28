c9553b973094b768618d98ce3d9946aae0c0abf5
thienvip <88622140+thienvip@users.noreply.github.com>
2023-06-28 18:14:15 +0700

    updated

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../shared/models/User';
+import { IUserLogin } from '../shared/interfaces/IUserLogin';
+import { HttpClient } from '@angular/common/http';
+import { USER_LOGIN_URL } from '../shared/constants/urls';
+
+const USER_KEY = 'User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private userSubject = new BehaviorSubject<User>(this.getUserFromLocalStorage())
+  public userObservable: Observable<User>
+
+  constructor(private http:HttpClient) {
+    this.userObservable = this.userSubject.asObservable()
+  }
+
+  private getUserFromLocalStorage():User{
+    const userJson = localStorage.getItem(USER_KEY);
+    if(userJson) return JSON.parse(userJson) as User
+    return new User()
+  }
+
+  login(userLogin : IUserLogin):Observable<User>{
+   return this.http.post<User>(USER_LOGIN_URL, userLogin).pipe(
+
+   )
+  }
+
+}
